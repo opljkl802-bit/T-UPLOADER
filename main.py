bc72bc26fb6cf5add17e2de46a67a54e54d3e6ac
opljkl802-bit <opljkl802@gmail.com>
2026-04-19 22:21:13 -0700

    Update main.py

--- main.py
+++ main.py
@@ -550,7 +550,7 @@ async def upload(bot: Client, m: Message):
     await input6.delete(True)
     await editable.delete()
 
-     thumb = input6.text
+     #thumb = input6.text
      if thumb.startswith("http://") or thumb.startswith("https://"):
          getstatusoutput(f"wget '{thumb}' -O 'thumb.jpg'")
          thumb = "thumb.jpg"
