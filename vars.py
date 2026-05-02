9ccf1bfffe07dd80217111d60141757d320e7bd9
opljkl802-bit <opljkl802@gmail.com>
2026-05-01 22:05:52 -0700

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,7 +1,8 @@
 import os
-from os import getenv
-# ---------------R---------------------------------
-API_ID = int(os.environ.get("API_ID", "30296254"))
+
+raw_api_id = os.environ.get("API_ID", "30296254")
+# This checks if the string is empty or None before converting
+API_ID = int(raw_api_id) if raw_api_id and raw_api_id.strip() else 30296254
 # ------------------------------------------------
 API_HASH = os.environ.get("API_HASH", "c2b5306f4ccd2d795405a026c10b4c62")
 # ----------------D--------------------------------
