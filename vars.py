71cc4856da75e58358c3604fb699d589e48ab66e
opljkl802-bit <opljkl802@gmail.com>
2026-05-02 19:31:05 -0700

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,9 +1,5 @@
 import os
 
-raw_api_id = os.environ.get("API_ID", "30296254")
-# This checks if the string is empty or None before converting
-API_ID = int(raw_api_id) if raw_api_id and raw_api_id.strip() else 30296254
-# ------------------------------------------------
-API_HASH = os.environ.get("API_HASH", "c2b5306f4ccd2d795405a026c10b4c62")
-# ----------------D--------------------------------
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8712933155:AAFMd0N2QyeAGMrRQt68NKDwVqBS8Skso6o")
+API_ID    = os.environ.get("API_ID", "")
+API_HASH  = os.environ.get("API_HASH", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
