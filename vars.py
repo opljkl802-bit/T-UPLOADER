834be99925af203367f69f2cbeeca64e40eb66a3
opljkl802-bit <opljkl802@gmail.com>
2026-05-05 01:39:14 -0700

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,5 +1,5 @@
 import os
 
-API_ID    = os.environ.get("API_ID", "")
-API_HASH  = os.environ.get("API_HASH", "")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+API_ID    = os.environ.get("API_ID", "30296254")
+API_HASH  = os.environ.get("API_HASH", "c2b5306f4ccd2d795405a026c10b4c62")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8614201321:AAFYogtipBDjlWBhTQGXJTcYuX3co4S0t4c") 
